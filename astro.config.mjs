ed7e325dd52ebf66c4f0556da56351a6ef6acdf0
zibrasismail <mohamedzibras2015@gmail.com>
2024-09-03 18:21:33 +0530

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: "https://benevolent-rolypoly-32615b.netlify.app/",
   integrations: [react()],
 });
